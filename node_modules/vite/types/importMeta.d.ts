dde94d1fd11598c3b4b12f225c87665842884a7d
김동현 <donghyun@gimdonghyeon-ui-MacBookPro.local>
2024-01-13 16:20:38 +0900

    add: boilerplate

--- node_modules/vite/types/importMeta.d.ts
+++ node_modules/vite/types/importMeta.d.ts
@@ -2,19 +2,13 @@
 // Thus cannot contain any top-level imports
 // <https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation>
 
-/* eslint-disable @typescript-eslint/consistent-type-imports */
-
-// Duplicate of import('../src/node/importGlob').GlobOptions in order to
-// avoid breaking the production client type. Because this file is referenced
-// in vite/client.d.ts and in production src/node/importGlob.ts doesn't exist.
-interface GlobOptions {
-  as?: string
-  /**
-   * @deprecated
-   */
-  assert?: {
-    type: string
-  }
+interface ImportMetaEnv {
+  [key: string]: any
+  BASE_URL: string
+  MODE: string
+  DEV: boolean
+  PROD: boolean
+  SSR: boolean
 }
 
 interface ImportMeta {
@@ -24,22 +18,5 @@ interface ImportMeta {
 
   readonly env: ImportMetaEnv
 
-  glob<Module = { [key: string]: any }>(
-    pattern: string,
-    options?: GlobOptions
-  ): Record<string, () => Promise<Module>>
-
-  globEager<Module = { [key: string]: any }>(
-    pattern: string,
-    options?: GlobOptions
-  ): Record<string, Module>
-}
-
-interface ImportMetaEnv {
-  [key: string]: any
-  BASE_URL: string
-  MODE: string
-  DEV: boolean
-  PROD: boolean
-  SSR: boolean
+  glob: import('./importGlob').ImportGlobFunction
 }
